d4547f7b6bcf34185b852e8ab8bf7f200eeafcf3
Joshua Vangor <joshua.vangor@gmail.com>
2025-11-10 12:49:28 -0500

    Updating Resume

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# reagsx.github.io
+# JoshVangor.github.io
 
 Uses resumed and jsonresume to generate a single page html resume, and pdf:
 
-resumed render chrisreagle.json --theme jsonresume-theme-even --output index.html
+resumed render joshvangor.json --theme jsonresume-theme-even --output index.html
--- scripts/output.sh
+++ scripts/output.sh
@@ -3,5 +3,5 @@ rm -rf output &&
 echo "creating new output" &&
 mkdir output && 
 echo "output deleted" &&
-resumed render chrisreagle.json --theme jsonresume-theme-even --output output/pdf_output.html &&
-resumed render chrisreagle.json --theme jsonresume-theme-even --output index.html 
+resumed render joshvangor.json --theme jsonresume-theme-even --output output/pdf_output.html &&
+resumed render joshvangor.json --theme jsonresume-theme-even --output index.html 
